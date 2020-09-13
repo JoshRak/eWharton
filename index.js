e7360916c6ce05533d747b2625bbbf93999e41b0
Josh <josh.rakovitsky@outlook.com>
2020-09-13 04:12:14 -0400

    back to pages links

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('docs'));
 
-app.get('/', function(req, res) {
-    res.send("assets/index.html");
-});
+// app.get('/', function(req, res) {
+//     res.send("assets/index.html");
+// });
 
-app.get('/index', function(req, res) {
-    res.send(path.join(__dirname + "index.html"));
-});
+// app.get('/index', function(req, res) {
+//     res.send(path.join(__dirname + "index.html"));
+// });
 
-app.get('/tour', function(req, res) {
-    res.send(path.join(__dirname + "tour.html"));
-});
+// app.get('/tour', function(req, res) {
+//     res.send(path.join(__dirname + "tour.html"));
+// });
 
-app.get('/features', function(req, res) {
-    res.send(path.join(__dirname + "features.html"));
-});
+// app.get('/features', function(req, res) {
+//     res.send(path.join(__dirname + "features.html"));
+// });
 
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
